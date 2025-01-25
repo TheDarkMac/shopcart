b95ad8aabe967f8b615f01bf79b5af3003eb1d79
hacktown <hei.ando.14@gmail.com>
2025-01-25 09:43:36 +0300

    feat: rerinitialize the structure cause of noobisme

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { ArticleCard } from './components/ArticleCard'
-import { Cart } from './components/Cart'
 import {imageURL as iURL} from './api/imageAPI.js';
 function App() {
   const [count, setCount] = useState(0);
@@ -14,14 +12,9 @@ function App() {
   return (
     <>
     <div>
-      <Cart/>
     </div>
     <div className='articleList'>
-
-      <ArticleCard  articleImage={imageURL} articlePrice={2} articleDescription='awda'/>
-      <ArticleCard articleName='nom' articleImage={imageURL} articlePrice={2} articleDescription='awda'/>
-      <ArticleCard articleName='nom' articleImage={imageURL} articlePrice={2} articleDescription='awda'/>
-    </div>
+ </div>
     </>
   )
 }
--- src/components/ArticleList.tsx
+++
@@ -1,9 +0,0 @@
-export function ArticleList({articleName}:{
-    articleName: string
-}){
-    return <>
-        <li>
-            {articleName}
-        </li>
-    </>
-}
